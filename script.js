d804e7258102d97d3ce19d9c9a46dfdbadb14e64
ItsJady-Taken <davidkhun2004@gmail.com>
2024-04-17 16:06:05 +0700

    Project progress: added win condition.
    Created a function to check if the player lose, win or tie.

--- script.js
+++ script.js
@@ -2,11 +2,55 @@
 // Create a list of options 
 const options = ["rock", "paper", "scissors"]
 
-function getComputerChoice () {
+// Get computer to choose randomly
+function getComputerChoice() {
     const computerChoice = options[Math.floor(Math.random() * options.length)];
-    return computerChoice;
+    return computerChoice
 }
 
+
+function playRound(playerSelection, computerSelection) {
+    // tie if have the same result
+    if(playerSelection === computerSelection) {
+        console.log("tie")
+    }
+    // if player choice rock
+    else if(playerSelection == 'rock'){
+        if(computerSelection == 'paper') {
+            console.log("You lose, paper beats rock.")
+        }
+        else if(computerSelection == 'scissors'){
+            console.log("You win, rock beats scissors.")
+        }
+    }
+    // if player choice paper
+    else if(playerSelection == 'paper'){
+        if(computerSelection == 'scissors') {
+            console.log("You lose, scissors beats paper.")
+        }
+        else if(computerSelection == 'rock'){
+            console.log("You win, paper beats rock.")
+        }
+    }
+    // if player choice scissors
+    else if(playerSelection == 'scissors'){
+        if(computerSelection == 'paper') {
+            console.log("You win, scissors beats paper.")
+        }
+        else if(computerSelection == 'rock'){
+            console.log("You lose, rock beats scissors.")
+        }
+    }
+    // result
+    return playerSelection, computerSelection;
+}
+
+  
+const playerSelection = "Scissors";
+const computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection.toLowerCase(), computerSelection));
+console.log("player: " + playerSelection)
 
 
 
